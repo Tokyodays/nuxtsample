dd90f3335086d6eb2f40f54747b91bf77231bf24
Tokyodays <kikuchi@gmail.com>
2020-03-21 14:57:31 +0900

    Update contentful.js

--- nuxt_blog/plugins/contentful.js
+++ nuxt_blog/plugins/contentful.js
@@ -13,6 +13,7 @@ const defaultConfig = (process.env.NODE_ENV === 'development') ? {
 
 module.exports = {
   contentfulCreateClient(config = defaultConfig) {
+    console.log(config.host)
     return contentful.createClient({
       space: config.CTF_SPACE_ID,
       accessToken: config.CTF_CDA_ACCESS_TOKEN,
